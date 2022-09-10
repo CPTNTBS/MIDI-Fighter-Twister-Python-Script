38faa61baf5032f228c6705c1afe9e7d76427168
CaptainTeebs <89997291+mb6wb@users.noreply.github.com>
2022-09-10 19:56:34 -0400

    Update device_MidiFighterTwister.py
    - moved appendix to end, added some more general terminology and reorganized appendix
    - When an encoder is linked to a value, the RGB LED is visible and Indicator Brightness is maxed. Unlinked encoders' LEDs are off and indicator brightness dimmed.

--- device_MidiFighterTwister.py
+++ device_MidiFighterTwister.py
@@ -1,96 +1,5 @@
 # name=Midi Fighter Twister
 
-"""
-#----------------------------------------GLOSSARY----------------------------------------#
-FUNCTIONALITY IN USE:
-- Switch Action Types:
-	- CC Toggle: Basically a button, turn on and turn off
-	- CC Hold: Basically a button, hold to turn on, turns off on release
-	- Reset Encoder Value: When Encoder Pressed, Value is reset to default value (50 with detent, 0 without)
-		How It Works:
-			- On a Control Change message sent to Channel 1 (Encoder Press), a value of 127 (0x7F) is sent.
-			- While this message is active, the value of data2 in Channel 0 is set to 0.
-			- On Release of the button, Channel 1 reverts to a value of 0.
-	- Shift Encoder Toggle: Basically two encoders in one, press the encoder to change between them.
-		How It Works:
-			- Encoder is on Channel 0 by default (Control Changes sent via 0xB0.
-			- A Control Change message of 127 is sent to Channel 1 (So the midiID is 0xB1, data2 is 0x7F)
-			- This Changes the Encoder to Channel 4, where the secondary set of encoder values are (0xB4)
-			- Sending another control change message to Channel 1 will send an OFF signal (0x00) to the encoder, reverting back to Channel 0.
-- Encoder MIDI Type:
-	- CC: Sends a CC value from 0-127
-	- ENC 3FH/41H: Sends continuous value w/o keeping track of value.
-
-USES:
-	- BI-DIRECTIONAL FEEDBACK
-		- Editing a value on MIDI Fighter Twister changes it on FL
-		- Editing a value in FL changes it on the lined encoder
-		- During playback, if automation is present on linked encoder, indicators will light up accordingly in RealTime
-		- When Recording during playback, can overwrite indicator automation via overdubbing.
-	- ENDLESS ENCODERS
-		- Endless Encoder functionality missing from Base MIDI Fighter Twister.
-		- When a value of 63 is given, linked control is reduced by 1.
-		- When a value of 65 is given, linked control is increased by 1.
-	- SMART SWITCH BETWEEN PLUGINS/GENERATORS
-		- When a GENERATOR/EFFECT is FOCUSED (window clicked on) in FL Studio, Any Linked Controls on the Midi Fighter Twister are Updated to those on the focused Generator/Effect.
-		- When Updating Midi Fighter Twister Indicators, last focused plugin is used.
-		- MAYBE? Change Color Scheme of all RGBs to preset one when focused on new effect/generator
-
-CHANNEL DATA:
-	- CHANNEL 0: INDICATOR AMOUNT (0 - 127)
-	- CHANNEL 1: RGB COLOR, ENDOCDER SHIFTER
-		- 0 = OFF State, 127 = ON State, any value between is considered ON, but will have a different color based on this: https://drive.google.com/file/d/0B-QvIds_FsH3Z0ZLT041VnZfOTA/view?resourcekey=0-fgp-m9PFslsMcR3SD5bJZQ
-		#SendMIDI(midi.MIDI_CONTROLCHANGE, 1, 15, COLOR.ORANGE) #Change Encoder 15 to Orange (on)
-	- CHANNEL 2: LED ANIMATION
-	- CHANNEL 3: BANK SELECT
-	- CHANNEL 4: SECONDARY ENCODER INDICATOR AMOUNT (SHIFT) (0 - 127)
-	- CHANNEL 5: LED ANIMATION (AGAIN?)
-	- CHANNEL 6: INDICATOR AMOUNT (AGAIN?)
-	- CHANNEL 8:
-
-LED ANIMATION CC VALUES:
-	- 0, 9, 48 = No Animation; Static
-	- 1 - 8 = RGB Flash
-	- 10 - 16 = RGB Pulse
-	- 17 - 47 = RGB Brightness (17 = Off, 32 = Mid, 47 = Max)
-	- 49 - 56 = Indicator Flash
-	- 57 - 64 = Indicator Pulse
-	- 65 - 95 = Indicator Brightness (65 = Off, 80 = Mid, 95 = Max)
-	- 127 = Rainbow Cycle
-
-CONTROL CHANGE ISH:
-	- 176 = B0 = Base Control Change Byte
-	- B0 + Channel = any other channel
-	
-
-SHIFT ENCODERS:
-	- SHIFT CC TYPE = B0, B4
-	- B1 = CONTROLCHANGE+1
-	
-MIDI FIGHTER TWISTER SETUP:
-	- BANK 1: Effects
-		- Bottom Right: Dry/Wet Mix
-	- BANK 2: Generator
-	- BANK 3: Misc. FL
-	- BANK 4: Free
-
-	#Universal Mix Control (Endless Encoder)
-	#event.data1 = any individual encoder/button/slider/misc; a control, data2 = CC Value (0-127)
-	#if the controller being modified is the mix on the sFighterTwister (First bank, last encoder)
-		#EndlessEncoder(event.data1, event.data2)
-
-HANDLING MIDI MESSAGES
-	- When a MIDI message is not handled, it doesn't control anything in FL/is not told to do anything in FL by the script/There is some task that it needs to do before it can be considered 'handled.'
-		- Using it will do the default ish that FL would make it do w/o the script.
-		Ex: Linking parameters to encoders, notes and their values, etc.
-	- Only handle parts of the device (channels, CCs, etc) when there is something specific that you want the control to do (change to this color when this happens, send this data back to the device when this happens, etc.).
-		- Some Channels and Settings (Channel 3, Reset Encoder Val) don't need to be handled b/c they more or less do things exclusively to the device itself and nothing to the DAW.
-
-Controls from 0-63
-Controls With Side button functionality: 0, 1, 2, 3, 8.,
-
-EventID = + param#
-"""
 import math
 import midi
 import device
@@ -102,12 +11,12 @@ import ui
 #----------------------------------------OVERRIDES----------------------------------------#
 """
 
-# When script first started
 def OnInit():
 	print(str(device.getName() + ' is connected to Port '+ str(device.getPortNumber())))
+	InitializedLEDs()
 	print("----------------------------------------------")
 
-	#for ctrlChange in range(63):
+		#	SendMIDI()
 		#print(device.getLinkedInfo(0x800000 + ctrlChange))
 		#if device.getLinkedInfo(0x800000 + ctrlChange) == -1:
 			#SendMIDI(midi.MIDI_CONTROLCHANGE, 2, ctrlChange, 17)
@@ -165,10 +74,47 @@ def OnMidiMsg(event):
 
 def OnRefresh(HW_Dirty_RemoteLinkValues):
 	GetFocusedWindowInfo()
-#	SendMIDI(
+
 """
 #----------------------------------------HELPER METHODS----------------------------------------#
 """
+#	Streamlined Midi Messaage sending to Device
+#		- command = CC Type (Control Change, Note On, Pitch Bend, etc)
+#		- channel = (Apperantly, not so sure) the channel that the device handles output you want
+#		- data1 = the individual controller (encoder, button, slider, led, etc) that you want to change (shift by 8).
+#		- data2 = the value you want to send. (bit shift by 16).
+def SendMIDI(command, channel, data1, data2):
+	device.midiOutMsg((command | channel) + (data1 << 8) + (data2 << 16));
+
+#Unlinked encoders have the LED turned off and Indicator Brightness Dimmed.
+def InitializedLEDs():
+	for ctrlChange in range(64):
+		print("Control #" + str(ctrlChange) + ": " + str(hex(device.findEventID(midi.EncodeRemoteControlID(device.getPortNumber(), 0, ctrlChange)))))
+		if device.findEventID(midi.EncodeRemoteControlID(device.getPortNumber(), 0, ctrlChange)) == 0x7fffffff:
+			SendMIDI(0xB0, 2, ctrlChange, 17)
+			SendMIDI(0xB0, 2, ctrlChange, 77)
+		else:
+			SendMIDI(0xB0, 2, ctrlChange, Animation.RGB_BRIGHT)
+			SendMIDI(0xB0, 2, ctrlChange, Animation.INDICATOR_BRIGHT)
+
+#Endless Encoder Fix
+#	- Made for ENC 3FH/41H mode.
+#		- When a value of 65 is given, the encoder sends a midi value of
+#			newValue = currentValue + 1
+#		- When a value of 63 is given, the encoder sends a midi value of
+#			newValue = currentValue - 1
+def EndlessEncoder(encVal):
+	if encVal == 65:
+		print('MixVal + 1')
+	elif encVal == 63:
+		print('MixVal - 1')
+
+#Get method for EventData
+def getEventID(track,slot,param):
+	return (((0x2000 + 0x40 * track) + slot) << 0x10) + 0x8000 + param
+
+
+#Debugging method for checking focused plugins
 def GetFocusedWindowInfo():
 	trackNumber = math.floor((ui.getFocusedFormID() >> 16) / 64)
 	slotNumber = (ui.getFocusedFormID() >> 16) % 64
@@ -187,10 +133,6 @@ def GetFocusedWindowInfo():
 	print("Plugin ID: " + str(hex(ui.getFocusedFormID())))
 	print("----------------------------------------------")
 
-
-
-
-#eventData.status the same as eventData.midiId + eventData.midiChan
 def GetChannel(encoder):
 	print(encoder.progNum)
 
@@ -208,57 +150,29 @@ def PrintEncoderData(encoder):
 
 #def ShiftControl(control):
 #	if control.midiId == midi.MIDI_CONTROLCHANGE + 1
-		
-
-#	Streamlined Midi Messaage sending to Device
-#		- command = CC Type (Control Change, Note On, Pitch Bend, etc)
-#		- channel = (Apperantly, not so sure) the channel that the device handles output you want
-#		- data1 = the individual controller (encoder, button, slider, led, etc) that you want to change (shift by 8).
-#		- data2 = the value you want to send. (bit shift by 16).
-def SendMIDI(command, channel, data1, data2):
-	device.midiOutMsg((command | channel) + (data1 << 8) + (data2 << 16));
-	
-'''
+			
+#Debugging method for checking controller event data
 def PrintOutControllerDeats(event):
 	#print('Modified Event is Handled: ' + str(bool(event.handled)))
 	#print('Modified Event Timestamp: ' + str(event.timestamp))
 	#print('Modified Event Status: ' + str(event.status))
 	if event.midiId == 0xB0:
-		print('Modified Event Type: Control Change')
+		print("Modified Event Type: Control Change")
 	elif event.midiId == 0x90:
-		print('Modified Event Type: Note On')
+		print("Modified Event Type: Note On")
 	else:
-		print('Modified Event Function: ' + str(event.midiId))
-	print('Modified Event Data1 (Controller): ' + str(event.data1))
-	print('Modified Event Data2 (Value): ' + str(event.data2))
-	print('Modified Event Port: ' + str(event.port))
-	print('Modified Event Channel: ' + str(event.midiChan))
-	print('Modified Event ChannelEX: ' + str(event.midiChanEx))
-	print('------------------------------------------------------')
-'''
-#	Endless Encoder Fix
-#		- Made for ENC 3FH/41H mode.
-#			- When a value of 65 is given, the encoder sends a midi value of
-#				newValue = currentValue + 1
-#			- When a value of 63 is given, the encoder sends a midi value of
-#				newValue = currentValue - 1
-def EndlessEncoder(controller, encVal):
-	if encVal == 65:
-		print('MixVal + 1')
-	elif encVal == 63:
-		print('MixVal - 1')
-		
-def getEventID(track,slot,param):
-	return (((0x2000 + 0x40 * track) + slot) << 0x10) + 0x8000 + param
+		print("Modified Event Function: " + str(event.midiId))
+	print("Modified Event Data1 (Controller): " + str(event.data1))
+	print("Modified Event Data2 (Value): " + str(event.data2))
+	print("Modified Event Port: " + str(event.port))
+	print("Modified Event Channel: " + str(event.midiChan))
+	print("Modified Event ChannelEX: " + str(event.midiChanEx))
+	print("----------------------------------------------")
 
 """
 #----------------------------------------CLASSES----------------------------------------#
 """
 	
-
-	
-
-
 class EncoderStatus:
 	ON = 127;
 	OFF = 0;
@@ -343,3 +257,101 @@ class Encoder:
 
 class EncoderBank:
 	channel3DefaultBank = 0;
+
+"""
+#----------------------------------------APPENDIX----------------------------------------#
+FUNCTIONALITY IN USE:
+	Switch Action Types Supported:
+		- CC Toggle: Basically a button, turn on and turn off
+		- CC Hold: Basically a button, hold to turn on, turns off on release
+		- Reset Encoder Value: When Encoder Pressed, Value is reset to default value (50 with detent, 0 without)
+			How It Works:
+				1. On a Control Change message sent to Channel 1 (Encoder Press), a value of 127 (0x7F) is sent.
+				2. While this message is active, the value of data2 in Channel 0 is set to 0.
+				3. On Release of the button, Channel 1 reverts to a value of 0.
+		- Shift Encoder Toggle: Basically two encoders in one, press the encoder to change between them.
+			How It Works:
+				1. Encoder is on Channel 0 by default (Control Changes sent via 0xB0.
+				2. A Control Change message of 127 is sent to Channel 1 (So the midiID is 0xB1, data2 is 0x7F)
+				3. This Changes the Encoder to Channel 4, where the secondary set of encoder values are (0xB4)
+				4. Sending another control change message to Channel 1 will send an OFF signal (0x00) to the encoder, reverting back to Channel 0.
+	Encoder MIDI Types Supported:
+		- CC: Sends a CC value from 0-127 to DAW
+		- ENC 3FH/41H: Sends continuous value w/o keeping track of value (by default this does not work in FL Studio).
+
+USES:
+	BI-DIRECTIONAL FEEDBACK
+		- Editing a value on MIDI Fighter Twister changes it on FL
+		- Editing a value in FL changes it on the lined encoder
+		- During playback, if automation is present on linked encoder, indicators will light up accordingly in RealTime
+		- When Recording during playback, can overwrite indicator automation via overdubbing.
+	ENDLESS ENCODERS
+		- Endless Encoder functionality missing from Base MIDI Fighter Twister.
+		- When a value of 63 is given, linked control is reduced by 1.
+		- When a value of 65 is given, linked control is increased by 1.
+	SMART SWITCH BETWEEN PLUGINS/GENERATORS
+		- When a GENERATOR/EFFECT is FOCUSED (window clicked on) in FL Studio, Any Linked Controls on the Midi Fighter Twister are Updated to those on the focused Generator/Effect.
+		- When Updating Midi Fighter Twister Indicators, last focused plugin is used.
+		- MAYBE? Change Color Scheme of all RGBs to preset one when focused on new effect/generator
+	OTHER ENHANCEMENTS
+		- When an encoder is linked to a value, the RGB LED is visible and Indicator Brightness is maxed. Unlinked encoders' LEDs are off and indicator brightness dimmed.
+
+CHANNEL DATA:
+	CHANNEL 0: INDICATOR AMOUNT (0 - 127)
+	CHANNEL 1: RGB COLOR, ENDOCDER SHIFTER
+		- 0 = OFF State, 127 = ON State, any value between is considered ON, but will have a different color based on MIDI Fighter Twister Manual, p. 4: https://drive.google.com/file/d/0B-QvIds_FsH3Z0ZLT041VnZfOTA/view?resourcekey=0-fgp-m9PFslsMcR3SD5bJZQ
+		- A class of basic colors is present in the code
+				Ex: SendMIDI(midi.MIDI_CONTROLCHANGE, 1, 15, COLOR.ORANGE) #Change Encoder 15 to Orange
+	CHANNEL 2: LED ANIMATION
+		Animation CC Values:
+			- 0, 9, 48 = No Animation; Static
+			- 1 - 8 = RGB Flash
+			- 10 - 16 = RGB Pulse
+			- 17 - 47 = RGB Brightness (17 = Off, 32 = Mid, 47 = Max)
+			- 49 - 56 = Indicator Flash
+			- 57 - 64 = Indicator Pulse
+			- 65 - 95 = Indicator Brightness (65 = Off, 80 = Mid, 95 = Max)
+			- 127 = Rainbow Cycle
+	CHANNEL 3: BANK SELECT
+	CHANNEL 4: SECONDARY (SHIFT) ENCODER INDICATOR AMOUNT (0 - 127)
+	CHANNEL 5: LED ANIMATION (AGAIN?)
+	CHANNEL 6: INDICATOR AMOUNT (AGAIN?)
+	CHANNEL 8:
+
+GENERAL INFORMATION:
+	CONTROL CHANGE MESSAGES
+		- 176 = B0 = Base Control Change Byte
+		- B0 + Channel = any other channel
+	SHIFT ENCODERS:
+		- SHIFT CC TYPE = B0 (Base Channel), B4 (Shift Channel)
+		- B1 = Default CC Channel that activates secondary channel
+	HANDLING MIDI MESSAGES
+		- When a MIDI message is not handled, it doesn't control anything in FL/is not told to do anything in FL by the script/There is some task that it needs to do before it can be considered 'handled.'
+			- Using it will do the default ish that FL would make it do w/o the script.
+			- Ex: Linking parameters to encoders, notes and their values, etc.
+		- Only handle parts of the device (channels, CCs, etc) when there is something specific that you want the control to do (change to this color when this happens, send this data back to the device when this happens, etc.).
+			- Some Channels and Settings (Channel 3, Reset Encoder Val) don't need to be handled b/c they more or less do things exclusively to the device itself and nothing to the DAW.
+	DEVICE CC VALUES
+		- CC Numbers on Device: 0-63 (64 different CCs)
+			- Controls With Side button functionality: 0, 1, 2, 3, 8.
+	PLUGINID COMPUTATION (EFFECT):
+		- Base 10: (track << 6 + slot) << 16
+		- Base 16: (track << 0x06 + slot) << 0x10
+	EVENTID COMPUTATION:
+		- Base 10: (((0x2000 + 0x40 * track) + slot) << 0x10) + 0x8000 + param
+		- Base 16: (((8192 + 64 * track) + slot) << 16) + 32768 + param
+	FL TERM GLOSSARY:
+		eventData = input from the device
+		data1 = device cc
+		data2 = device cc amount
+		midiId = midi codes (see midi.py)
+		midiChan = midi channel
+		status = midiId + midiChan
+
+TENTATIVE MIDI FIGHTER TWISTER BANK SETUP:
+	BANK 1: Effects
+		- Bottom Right: Dry/Wet Mix
+	BANK 2: Generator
+	BANK 3: Misc. FL
+	BANK 4: Free
+"""
